8fb6351ef736b19edd4626c98f81ec738fecf554
Dasari-Pavan <pvnkmrdsr@gmail.com>
2024-07-25 14:19:50 +0530

    Episode 3.1

--- App.js
+++ App.js
@@ -1,18 +1,42 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDom from "react-dom/client"
 
-const parent=React.createElement("div",{id:"parent"},
-       [ React.createElement("div",{id:"child1"},
-        [React.createElement("h1",{},"I'm h1 tag"),React.createElement("h2",{},"I'm h2 tag")]
-        ),
-        React.createElement("div",{id:"child2"},
-        [React.createElement("h1",{},"I'm h1 tag"),React.createElement("h2",{},"I'm h2 tag")]
-        )]
-);
-     
-const root=ReactDOM.createRoot(document.getElementById("root"))
+// React.createElement => object =>HTMLElement(render)
 
-root.render(parent);
+// const heading=React.createElement("h1",{id:"heading"},"Parent element");
 
+// we need to jsx not  jsx =>react.createlement =>react.object => render htmml
+
+const jsxheading= <h1 className="ead" tabIndex="5">React using jsx syntax</h1>;
+
+// console.log(heading);
+// const root=ReactDom.createRoot(document.getElementById("root"));
+// root.render(jsxheading);
 
 
+// React Element
+const Head=(
+  <h1 className="head" tabIndex="5">
+    Namaste React using react element
+  </h1>
+)
+
+// functional element
+
+const Heading=()=>(
+  <h1 className="head" tabIndex="5">
+    Namaste React using functional
+  </h1>
+)
+// functional component
+const HeadingComponent=()=>(
+  <div className="container">
+    {Head}
+    <Heading/>
+  <h1>Reactjs using Jsx via functional component</h1>
+  </div>
+)
+
+const root=ReactDom.createRoot(document.getElementById("root"));
+root.render(<HeadingComponent/>);
+// root.render(heading);
